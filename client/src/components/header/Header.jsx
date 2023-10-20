5edca738201870ebb160dca134a35d163b39eb50
Vicky-peace <victorpeace47@gmail.com>
2023-10-20 05:25:44 +0300

    new task

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import './header.css';
+import {FaBed} from 'react-icons/fa'
+import {MdFlight} from 'react-icons/md';
+import {AiFillCar} from 'react-icons/ai'
+import {BsFillTaxiFrontFill} from 'react-icons/bs'
 const Header = () => {
   return (
     <div className='header'>
         <div className="headerList">
-            <div className="headerListItem">
+            <div className="headerListItem active">
+           <FaBed/>
                 <span>Stays</span>
             </div>
            
             <div className="headerListItem">
+                <MdFlight/>
                 <span>Flights</span>
             </div>
             <div className="headerListItem">
+                <AiFillCar/>
                 <span>Car Rentals</span>
             </div>
             <div className="headerListItem">
-                <span>Accomodations</span>
-                <div className="headerListItem">
-                <span>Airport Taxis</span>
+                <FaBed/>
+                <span>Attraction</span>
+               
             </div>
+            <div className="headerListItem">
+                    <BsFillTaxiFrontFill/>
+                <span>Airport Taxis</span>
             </div>
         </div>
     </div>
